84ff7b2c7c15641bb4ab8969b54d9ec5c374d66a
jordimall <jordi.mallafre@hotmail.com>
2023-10-10 16:31:10 +0200

    Implementación de los getters, setters, toString y metodo libroConMasPaginas

--- src/Ejercicio3/Libro.java
+++ src/Ejercicio3/Libro.java
@@ -13,12 +13,12 @@ package Ejercicio3;
  * @author Jordi Mallfre & David Maza
  */
 public class Libro {
-	
+
 	private int isbn;
 	private String titulo;
 	private String autor;
 	private int num_paginas;
-	
+
 	public Libro(int isbn, String titulo, String autor, int num_paginas) {
 
 		this.isbn = isbn;
@@ -27,8 +27,63 @@ public class Libro {
 		this.num_paginas = num_paginas;
 	}
 
-	
-	// TODO Getters and Settes
-	// TODO toStrig()
-	// TODO libroConMasPaguinas()
+	public int getIsbn() {
+		return isbn;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public int getNum_paginas() {
+		return num_paginas;
+	}
+
+	public void setIsbn(int isbn) {
+		this.isbn = isbn;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public void setNum_paginas(int num_paginas) {
+		this.num_paginas = num_paginas;
+	}
+
+	@Override
+	public String toString() {
+		return "El " + this.titulo + " con el ISBN: " + this.isbn + " creado por el autor " + this.autor + " tiene "
+				+ this.num_paginas + " páginas";
+	}
+
+	/**
+	 * Compara el número de páginas de el objeto Libro pasado por parametro con el
+	 * actual y en función del resultado muestra un mensaje por consola
+	 * 
+	 * @param Libro
+	 */
+	public void libroConMasPaguinas(Libro libro) {
+
+		if (this.num_paginas < libro.getNum_paginas()) {
+			System.out.println("El libto con mas páginas es: \n");
+			System.out.println(libro);
+		} else if (this.num_paginas > libro.getNum_paginas()) {
+			System.out.println("El libto con mas páginas es: \n");
+			System.out.println(this.toString());
+		} else {
+			System.out.println("Ambos libros tiene el mismo numero de páginas \n");
+			System.out.println(this.toString());
+			System.out.println();
+			System.out.println(libro);
+		}
+	}
 }
