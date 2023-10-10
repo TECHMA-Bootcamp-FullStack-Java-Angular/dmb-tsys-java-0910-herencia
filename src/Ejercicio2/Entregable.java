6e9ce7937c48e2844115d5fbd829db149c733f20
jordimall <jordi.mallafre@hotmail.com>
2023-10-10 10:39:36 +0200

    Implementación de la interfaz Entregable

--- src/Ejercicio2/Entregable.java
+++ src/Ejercicio2/Entregable.java
@@ -1,5 +1,28 @@
 package Ejercicio2;
 
+/**
+ * Interfaz Entregable
+ * @author Jordi Mallafre
+ */
 public interface Entregable {
+	
+	//Cambia el contenido de la variable entregado a true	
+	public void entregar();
 
+	//Cambia el contenido de la variable entregado a false
+	public void devolver();
+
+	//devuelve el contenido de la variable entregado 
+	public boolean isEntregado();
+
+	/**
+	 * Parsea el Objeto a a la clase pertinente.
+	 * Compara la variable horas_estimadas o temporadas en función de la clase con la del objeto pasado por parametro
+	 * Devuelve 0 si son iguales
+	 * devuelve 1 si la variable del objeto pasado por parametro es mas pequeño
+	 * devuelve -1 si la variable del objeto pasado por parametro es mas grande
+	 * @param a
+	 * @return int
+	 */
+	public int compareTo(Object a);
 }
