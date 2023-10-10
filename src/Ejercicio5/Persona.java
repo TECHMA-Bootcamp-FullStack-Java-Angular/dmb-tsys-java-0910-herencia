411fd99345a03cfc76421599018582fabf9c12ba
David Maza Borjabad <109163247+d-maza@users.noreply.github.com>
2023-10-10 14:43:34 +0200

    add new class Profesor

--- src/Ejercicio5/Persona.java
+++ src/Ejercicio5/Persona.java
@@ -1,5 +1,18 @@
 package Ejercicio5;
 
 public class Persona {
+	
+	protected String nombre;
+	protected int edad;
+	protected String sexo;
+	protected int calificacion;
+	
+	public Persona(String nombre, int edad, String sexo, int calificacion) {
 
+		this.nombre = nombre;
+		this.edad = edad;
+		this.sexo = sexo;
+		this.calificacion = calificacion;
+	}
+	
 }
--- src/Ejercicio5/Profesor.java
+++ src/Ejercicio5/Profesor.java
@@ -1,5 +1,18 @@
 package Ejercicio5;
 
-public class Profesor {
+public class Profesor extends Persona {
 
+	public Profesor(String nombre, int edad, String sexo, int calificacion) {
+		super(nombre, edad, sexo, calificacion);
+	}
+
+	public boolean asistirAula() {
+		// TODO
+		return false;
+	}
+
+	public String alumnoAprobado() {
+		// TODO
+		return "";
+	}
 }
