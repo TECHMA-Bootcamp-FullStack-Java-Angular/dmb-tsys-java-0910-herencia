c7be2a47c47ef3f4bd14fdf24b271a9b5d52894c
David Maza Borjabad <109163247+d-maza@users.noreply.github.com>
2023-10-13 14:01:20 +0200

    se añden cambios de  class estudiante

--- src/Ejercicio5/Estudiante.java
+++ src/Ejercicio5/Estudiante.java
@@ -10,45 +10,44 @@ import java.util.Random;
  * @author David Maza
  */
 class Estudiante extends Persona {
-	
-    private double calificacionActual;
 
-    public Estudiante(String nombre, int edad, String sexo, double calificacionActual) {
-        super(nombre, edad, sexo);
-        this.calificacionActual = calificacionActual;
-    }
+	private double calificacionActual;
 
-    public double getCalificacionActual() {
-        return calificacionActual;
-    }
+	public Estudiante(String nombre, int edad, String sexo, double calificacionActual) {
+		super(nombre, edad, sexo);
+		this.calificacionActual = calificacionActual;
+	}
 
-    /**
-     * Devuelve si el profesor está ausente o no.
+	public double getCalificacionActual() {
+		return calificacionActual;
+	}
 
-     *
-     * @return `true` si el profesor está ausente, `false` si está disponible
-     */
-    public boolean faltar() {
-    	 Random random = new Random();
+	/**
+	 * Devuelve si el profesor está ausente o no.
+	 *
+	 * 
+	 * @return `true` si el profesor está ausente, `false` si está disponible
+	 */
+	public boolean faltar() {
+		Random random = new Random();
 		return random.nextDouble() < 0.5 ? true : false;
-    }
+	}
 
-    /**
-     * Devuelve si el estudiante está aprobado o no.
-     *
-     * @return `true` si el estudiante está aprobado, `false` si está reprobado
-     */
-    public boolean estaAprobado() {
-        return calificacionActual >= 5.0;
-    }
+	/**
+	 * Devuelve si el estudiante está aprobado o no.
+	 *
+	 * @return `true` si el estudiante está aprobado, `false` si está reprobado
+	 */
+	public boolean estaAprobado() {
+		return calificacionActual >= 5.0;
+	}
 
-    
-    /**
-     * Devuelve la calificación actual del estudiante formateada a dos decimales.
-     *
-     * @return la calificación actual del estudiante formateada a dos decimales
-     */
-    public String getCalificacionFormateada() {
-    	  return String.format("%.2f", Math.round(calificacionActual * 1000.0) / 1000.0);
-    	}
+	/**
+	 * Devuelve la calificación actual del estudiante formateada a dos decimales.
+	 *
+	 * @return la calificación actual del estudiante formateada a dos decimales
+	 */
+	public String getCalificacionFormateada() {
+		return String.format("%.2f", Math.round(calificacionActual * 1000.0) / 1000.0);
+	}
 }
--- src/Ejercicio5/Profesor.java
+++ src/Ejercicio5/Profesor.java
@@ -13,59 +13,57 @@ import java.util.Random;
  * @author David Maza
  */
 class Profesor extends Persona {
-	
-    private String materia;
-    static Random random = new Random();
 
-    public Profesor(String nombre, int edad, String sexo, String materia) {
-        super(nombre, edad, sexo);
-        this.materia = materia;
-    }
+	private String materia;
+	static Random random = new Random();
 
-    public String getMateria() {
-        return materia;
-    }
-    
-    /**
-     * Devuelve si el profesor está ausente o no.
-     *
-     * @return `true` si el profesor está ausente, `false` si está disponible
-     */
-    public boolean faltar() {
-    	return random.nextDouble()< 0.2 ? true : false;
-    }
-    
-    /**
-     * Crea una lista de profesores con un número aleatorio de profesores por materia.
-     *
-     * @param materias las materias para las que se crearán profesores
-     * @return una lista de profesores
-     */
-    public static ArrayList<Profesor> crearProfesores(String[] materias)
-    {
-        ArrayList<Profesor> profesores = new ArrayList<>();
-        
-        Arrays.stream(materias).forEach(e -> {
-        	
-        	Profesor profesor = new Profesor("Profesor " + e, random.nextInt(31),random.nextBoolean() ? "Hombre" : "Mujer",e);
-        	profesores.add(profesor.faltar() ? null : profesor);
-        	
-        });
+	public Profesor(String nombre, int edad, String sexo, String materia) {
+		super(nombre, edad, sexo);
+		this.materia = materia;
+	}
 
-        return profesores;
-    }
+	public String getMateria() {
+		return materia;
+	}
 
-    /**
-     * Obtiene el primer profesor disponible para la materia especificada.
-     *
-     * @param profesores la lista de profesores
-     * @param materia la materia para la que se busca un profesor disponible
-     * @return el primer profesor disponible para la materia especificada, o `null` si no se encuentra ningún profesor disponible
-     */
-    public static Profesor obtenerProfesorDisponible(List<Profesor> profesores, String materia) {
-    	  return profesores.stream()
-    	    .filter(profesor -> profesor.getMateria().equals(materia))
-    	    .findFirst()
-    	    .orElse(null);
-    	}
+	/**
+	 * Devuelve si el profesor está ausente o no.
+	 *
+	 * @return `true` si el profesor está ausente, `false` si está disponible
+	 */
+	public boolean faltar() {
+		return random.nextDouble() < 0.2 ? true : false;
+	}
+
+	/**
+	 * Crea una lista de profesores con un número aleatorio de profesores por
+	 * materia.
+	 *
+	 * @param materias las materias para las que se crearán profesores
+	 * @return una lista de profesores
+	 */
+	public static ArrayList<Profesor> crearProfesores(String[] materias) {
+		ArrayList<Profesor> profesores = new ArrayList<>();
+
+		Arrays.stream(materias).forEach(e -> {
+
+			Profesor profesor = new Profesor("Profesor " + e, random.nextInt(31),random.nextBoolean() ? "Hombre" : "Mujer", e);
+			profesores.add(profesor.faltar() ? null : profesor);
+
+		});
+
+		return profesores;
+	}
+
+	/**
+	 * Obtiene el primer profesor disponible para la materia especificada.
+	 *
+	 * @param profesores la lista de profesores
+	 * @param materia    la materia para la que se busca un profesor disponible
+	 * @return el primer profesor disponible para la materia especificada, o `null`
+	 *         si no se encuentra ningún profesor disponible
+	 */
+	public static Profesor obtenerProfesorDisponible(List<Profesor> profesores, String materia) {
+		return profesores.stream().filter(profesor -> profesor.getMateria().equals(materia)).findFirst().orElse(null);
+	}
 }
